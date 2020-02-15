f15c77a375a920711424ff87fe51bbeefd980d05
RAFAEL HENRIQUE DE MORAIS <22321@unifacef.edu.br>
2020-02-15 16:44:54 -0300

    Convertendo calculo de etanol e gasolina

--- index.tsx
+++ index.tsx
@@ -1,10 +1,24 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
 import 'react-native-gesture-handler';
 
-AppRegistry.registerComponent(appName, () => App);
+import * as Stores from './src/stores';
+
+import { AppRegistry, SafeAreaView } from 'react-native';
+import { light as lightTheme, mapping } from '@eva-design/eva';
+
+import { ApplicationProvider } from '@ui-kitten/components';
+import { Provider } from 'mobx-react';
+import React from 'react';
+import Routes from './src/routes';
+import { name as appName } from './app.json';
+
+const ProviderConfigured = () => (
+    <Provider {...Stores}>
+        <ApplicationProvider mapping={mapping} theme={lightTheme}>
+            <SafeAreaView style={{ flex: 1 }}>
+                <Routes />
+            </SafeAreaView>
+        </ApplicationProvider>
+    </Provider>
+)
+
+AppRegistry.registerComponent(appName, () => ProviderConfigured);
